133f95bbdaf0013e7216a1dcb773e59cc8d9d070
Ealfred1 <alfrederic371@gmail.com>
2024-08-14 00:52:23 +0100

    mobile nav ready

--- src/components/NavBar/MobileNav/MobileNav.jsx
+++ src/components/NavBar/MobileNav/MobileNav.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import { Link } from 'react-router-dom'
 
 const MobileNav = ({ isOpen, toggleMenu }) => {
 	return (
@@ -10,11 +10,11 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
 						<h1 className="">RealTimeAstro</h1>
 					</div>
 					<ul className="flex flex-col gap-4 ml-2 space-y-4">
-						<li className="navlink"><Link href="#home">Home</a></li>
-						<li className="navlink"><Link href="#services">Services</a></li>
-						<li className="navlink"><Link href="#projects">Projects</a></li>
-						<li className="navlink"><Link href="#skills">Skills</a></li>
-						<li className="navlink"><Link href="#contact">Contact Me</a></li>
+						<li className="navlink"><Link to="#home">Horoscope</Link></li>
+						<li className="navlink"><Link to="#services">Astro-Tools</Link></li>
+						<li className="navlink"><Link to="#projects">Services</Link></li>
+						<li className="navlink"><Link to="#skills">Resources</Link></li>
+						<li className="navlink"><Link to="#contact">Community</Link></li>
 					</ul>
 				</div>
 			</div>
