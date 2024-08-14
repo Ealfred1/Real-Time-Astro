20a44e15c233944e2d490c6fd2298abe5a3a4701
Ealfred1 <alfrederic371@gmail.com>
2024-08-14 01:37:26 +0100

    section A progress

--- src/components/Team/SectionA.jsx
+++ src/components/Team/SectionA.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const SectionA = () => {
 	return (
-		<section className="">
-			
+		<section className="w-full flex items-center flex-col">
+			<div className="flex items-center flex-col w-1/2">
+				<div className="mb-2">
+					<h1 className="text-secondary text-[4.5rem] font-serif font-semibold">Astro-Team</h1>
+				</div>
+				<div className="max-w-prose text-center">
+					<p className="">
+						Learn from our experts readers and be guided on your transformative<br /> journey of self-discovery through the powerful wisdom of astrology.
+					</p>
+				</div>
+			</div>
 		</section>
 	)
 }
