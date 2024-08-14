313296a5a070cf7f45f48031df7854d563b53aa2
Ealfred1 <alfrederic371@gmail.com>
2024-08-14 15:36:47 +0100

    CHANGES

--- src/components/Team/Testimonials.jsx
+++ src/components/Team/Testimonials.jsx
@@ -5,7 +5,7 @@ import data from '../../data/teamData2.json'
 const Testimonials = () => {
 	return (
 		<section className="w-full px-2 flex flex-col items-center justify-center pt-32">
-			<div className="flex items-center justify-center text-center">
+			<div className="flex items-center justify-center text-center mb-9">
 				<h2 className="uppercase text-7xl">What our clients<br /> say about us</h2>
 			</div>
 
@@ -20,10 +20,18 @@ const Testimonials = () => {
 						<div className="w-full flex flex-col items-center justify-around mt-5 gap-4">
 							<img src={user.img} alt="" className="w-[10rem] h-[10rem] rounded-full object-cover" />
 							<div className="">
-								<h2 className="text-center">{user.name}</h2>
-								<p className="">{user.work}</p>
+								<h2 className="text-center tracking-wide text-tertiary">{user.name}</h2>
+								<p className="text-sm text-center tracking-tight">{user.work}</p>
 							</div>
 						</div>
+
+						<div className="w-full text-secondary text-right p-2 flex items-center justify-center mt-3 space-x-1">
+			            <i className="pi pi-star-fill"></i>
+			            <i className="pi pi-star-fill"></i>
+			            <i className="pi pi-star-fill"></i>
+			            <i className="pi pi-star-fill"></i>
+			            <i className="pi pi-star-fill"></i>
+			          </div>
 
 					</div>
 				))}
